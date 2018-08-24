ec99f8755bc594e227fd12bc6e7244326dc01164
John Witham <john.h.witham@gmail.com>
2018-08-24 13:58:20 +0000

    Add model Category & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180823115939) do
+ActiveRecord::Schema.define(version: 20180824135544) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plays", force: :cascade do |t|
     t.string   "title"
